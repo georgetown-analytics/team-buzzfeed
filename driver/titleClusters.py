e0346f69b70c434de91b332babfd88ce73f83f31
Josh <josherb4@gmail.com>
2016-07-05 13:58:59 -0400

    added some incomplete code and some sample data to my branch

--- driver/titleClusters.py
+++ driver/titleClusters.py
@@ -17,20 +17,38 @@ import gensim
 ## Module Variables/Constants
 ##########################################################################
 
-
+Path = ''
 
 ##########################################################################
 ## Functions
 ##########################################################################
+def fetch_buzzes():
+    for article in
+
+def genCorpus():
+    """
+    Takes all titles from available data and dumps them into a single .txt
+    file to be used as a corpus.
+    """
+    Buzzes = fetch_buzzes()
+    for title in Buzzes:
+        path = os.path.join('../tests/data', '{}.json'.format(article))
+        with open(path, 'w') as f:
+            f.write(buzzes['title'], '\n')
+
 def articleType():
-    ##
+    """
+    Guesses the article type based on the contents of the title
+    """
+    pass
 
 def lexicalDiversity():
-    ## need to take the number of unique words for all titles and divide
+    """Takes a corpus and computes number of unique words
+    divided by total words"""
     ## by the total number of words for all titles.
 
 def main():
-
+    for buzz in
 
 ##########################################################################
 ## Execution
