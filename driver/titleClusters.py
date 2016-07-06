9bc80d000e66e534d9715cc94997b4134abe4b4e
Josh <josherb4@gmail.com>
2016-07-06 17:52:18 -0400

    figured out file_iterator()

--- driver/titleClusters.py
+++ driver/titleClusters.py
@@ -12,12 +12,16 @@ import os
 import glob
 import json
 import requests
+from pprint import pprint
 
 ##########################################################################
 ## Module Variables/Constants
 ##########################################################################
 
+#where all my dummy files are located
 dataPath = glob.glob('./tests/data/*.txt')
+# initializing nested dictionary that will house all titles and instance counts
+masterDic = {'us': {'':0}, 'ca': {'':0}, 'uk': {'':0}, 'in': {'':0}, 'au': {'':0}}
 
 ##########################################################################
 ## Functions
@@ -38,12 +42,29 @@ def grab_titles(filename): # this one's functioning properly!
 
     return title_list
 
-def file_iterator():
+def file_iterator(country, dirPath):
     """
-    Uses a janky while loop to iterate over files and uses grab_titles() to
-    generate a dictionary organized by country
+    Uses a janky loop to iterate over files and uses grab_titles() to
+    generate a dictionary (with title lists) based on the country you entered.
+    Feeds into genCorpus() nicely.
     """
-    pass
+    if country in masterDic: #quick check to make sure we've got an actual country code to work with
+        for doc in dirPath: #for loop iterates over all files in dirPath list
+            # grab the titles and dump them into a list
+            country_titles = grab_titles(doc)
+            # Read every title and add any that aren't in masterDic to masterDic
+            for item in country_titles: #looks at every title in the list
+                #if it's not there, add new title to dictionary
+                if item not in masterDic[country]:
+                    masterDic[country].update({item: 1})
+                #if it is there, add one to the instance ticker
+                else:
+                    original_val = masterDic[country][item]
+                    new_val = original_val + 1
+                    masterDic[country].update({item: new_val})
+        return masterDic #hands back the populated dictionary for whatever use you see fit. Like generating a title corpus, perhaps?
+    else:
+        print('You\'ve got to specify an actual country code. Hint: only two characters')
 
 def genCorpus(country):
     """
@@ -51,6 +72,10 @@ def genCorpus(country):
     into a single .txt file based on the country you enter.
     Giving you a country-specific corpus.
     """
+    # new_filename = str(country) #make sure it's a string
+    # (NOTHING TO SEE HERE)
+    # with open(new_filename, 'w') as f:
+    #   print(STUFF)
     pass
 
 def articleType():
@@ -65,7 +90,8 @@ def lexicalDiversity(corpus):
     pass
 
 def main():
-    grab_titles(dataPath[5099]) # just running a simple test here
+    test = input("Enter country code:").lower() # just running a simple test here
+    file_iterator(test, dataPath)
 
 ##########################################################################
 ## Execution
